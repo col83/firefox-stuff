22b27ebd51501e6843f291d4f44d8923580f1faa
dashi <181648839+col83@users.noreply.github.com>
2026-05-22 20:45:31 +0300

    update & refactor sort-noscript-domains-jq.bat
    added [Notepad++](https://github.com/notepad-plus-plus/notepad-plus-plus) integration

--- setup/ext-settings/noscript/sort-noscript-domains-jq.bat
+++ setup/ext-settings/noscript/sort-noscript-domains-jq.bat
@@ -1,3 +1,15 @@
-powershell -NoProfile -ExecutionPolicy Bypass -File "sort-noscript-domains-jq.ps1" "noscript_data.txt"
+@echo off
+
+set "NOSCRIPT_FILE=noscript_data.txt"
+
+set "DOMAINS_SORT_SCRIPT=sort-noscript-domains-jq.ps1"
+
+powershell -NoProfile -ExecutionPolicy Bypass -File "%DOMAINS_SORT_SCRIPT%" "%NOSCRIPT_FILE%"
 
 pause
+
+set "EDITOR=%ProgramFiles%\Notepad++\notepad++.exe"
+
+if exist "%EDITOR%" (
+    start "" "%EDITOR%" "%NOSCRIPT_FILE%"
+)
