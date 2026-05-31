22f17791a9f3e69eb39af84b7ce9f23f59839b01
dashi <181648839+col83@users.noreply.github.com>
2026-05-31 19:35:04 +0300

    update

--- setup/clean_bookmarks_full.bat
+++ setup/clean_bookmarks_full.bat
@@ -1,1 +1,7 @@
 powershell -NoProfile -ExecutionPolicy Bypass -File "clean_bookmarks_full.ps1" "bookmarks.html"
+
+set "EDITOR=%ProgramFiles%\Notepad++\notepad++.exe"
+
+if exist "%EDITOR%" (
+    start "" "%EDITOR%" "bookmarks.html"
+)
